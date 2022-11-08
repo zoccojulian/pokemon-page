d47dcc85372950cea9eb75895b7621979ffa98f3
zoccojulian <zoccojulian@gmail.com>
2022-11-08 14:45:21 -0300

    Correccion bug en localStorage de posicion de la lista

--- src/component/container/ListaPokemon.jsx
+++ src/component/container/ListaPokemon.jsx
@@ -5,12 +5,14 @@ import Card from '../pure/Card';
 import '../../scss/lista_pokemon.scss';
 
 
-const initialState =  JSON.parse(localStorage.getItem('pokemonListaPosicion')) || { lista: [], previous: '', next: '', numero: { desde: '0', hasta: '20'} };
+
 
 let numeroPorPagina = 20;
 
 
 export default function ListaPokemon() {
+
+    const initialState =  JSON.parse(localStorage.getItem('pokemonListaPosicion')) || { lista: [], previous: '', next: '', numero: { desde: '', hasta: ''} };
     
     const [pokemonPagina, setPokemonPagina] = useState(initialState);
 
@@ -53,15 +55,21 @@ export default function ListaPokemon() {
 
     useEffect(() => {
         
-        cargarPokemons( `${URL_POKEMON}/?offset=${initialState.numero.desde}&limit=${numeroPorPagina}` );
-
-    }, []);
+        let offsetParaPeticion = pokemonPagina.next.replace('https://pokeapi.co/api/v2/pokemon/?offset=', '').replace(`&limit=${numeroPorPagina}`,'');
+        console.log(offsetParaPeticion)
+        cargarPokemons( `${URL_POKEMON}/?offset=${offsetParaPeticion-20}&limit=${numeroPorPagina}` );
+    }, [ ]);
 
 
     return (
         <div className='lista__pokemon-container'>
             <h2 className='lista__pokemon-titulo'>Lista</h2>
             <div>
+                { pokemonPagina.previous !== null && (
+                    <button
+                    onClick={ () => cargarPokemons( `${URL_POKEMON}/?offset=$0&limit=${numeroPorPagina}` ) }
+                >INICIO</button>
+                ) }
                 { pokemonPagina.previous !== null && (
                     <button
                     onClick={ () => cargarPokemons( pokemonPagina.previous ) }
